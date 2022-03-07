0ace77917a0764efda79c8b93e7429f18e54387d
huberc <carolin-elisabeth.huber@ufz.de>
2022-03-07 18:21:02 +0100

    clean-up, run until library search

--- R/04_MS2Annotation.R
+++ R/04_MS2Annotation.R
@@ -1,8 +1,41 @@
 #' Function for performing spectral library search with MS2 data
 #'
-#' @param project_dir Path to project directory
+#' @param se QFeature SummarizedExperiment
+#' @param query Spectra object containing MS2
+#' @param output_dir Filepath to output directory
+#' @param settings Settings parameter list
 #' 
-MS2_annotation <- function(project_dir){
+MS2Annotation <- function(se, query, output_dir, settings){
     
-    
+    #Perform external library search without RT
+    if(nchar(settings$MS2_lib_ext)>0){
+        se <- librarysearch_se(se, query, 
+                           library_dir=settings$MS2_lib_ext, 
+                           library_format = settings$MS2_lib_ext_format,
+                           output_dir = output_dir,
+                           output_name = "extern",
+                           annotated_only=settings$annotated_only, 
+                           dp_tresh=settings$dp_tresh, 
+                           mz_ppm=settings$mz_ppm,
+                           int_tresh=settings$int_tresh,
+                           compare_rt=FALSE,
+                           rt_thresh_min=settings$rt_thresh_min,
+                           plot_headtail=settings$plot_headtail)
+    }
+    #Perform in-house library search with RT
+    if(nchar(settings$MS2_lib_inhouse)>0){
+        se <- librarysearch_se(se, query, 
+                           library_dir=settings$MS2_lib_inhouse, 
+                           library_format = settings$MS2_lib_inhouse_format,
+                           output_dir = output_dir,
+                           output_name = "inhouse",
+                           annotated_only=settings$annotated_only, 
+                           dp_tresh=settings$dp_tresh, 
+                           mz_ppm=settings$mz_ppm,
+                           int_tresh=settings$int_tresh,
+                           compare_rt=TRUE,
+                           rt_thresh_min=settings$rt_thresh_min,
+                           plot_headtail=settings$plot_headtail)
+    }
+    return(se)   
 }
---
+++ workflow_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+Rscript workflow.R "testoutput" "testdata/settings_MetaboAnnotation.yaml"
+
