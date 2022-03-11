f744c9bc36fd1bcf90cc7533e6c1809ca5063d51
michaelwitting <michael.witting@helmholtz-muenchen.de>
2022-03-11 15:49:54 +0100

    changed workflow until MS1 annotation

--- R/00_Setup.R
+++ R/00_Setup.R
@@ -1,62 +1,46 @@
-#' Function for setting up project folder and reading in settings file
-#'
-#' @param settings_yaml Path to a yaml file which should be used and copied to project directory
-#' 
-#' @return settings
-prepare_setup <- function(settings_yaml){
-    
-    # Load all required packages
-    {
-        if (!require("BiocManager", quietly = TRUE))
-            install.packages("BiocManager")
-        
-        if (!require("MetaboAnnotation")) devtools::install_github("rformassspectrometry/MetaboAnnotation")
-        library(MetaboAnnotation)
-        
-        if (!require("MsBackendMassbank")) BiocManager::install("MsBackendMassbank")
-        library(MsBackendMassbank)
-        
-        if (!require("MsBackendMgf")) BiocManager::install("MsBackendMgf")
-        library(MsBackendMgf)
-        
-        if (!require("MsBackendMsp")) BiocManager::install("MsBackendMsp")
-        library(MsBackendMsp)
-        
-        if (!require("Spectra")) BiocManager::install("Spectra")
-        library(Spectra)
-        
-        if (!require("QFeatures")) BiocManager::install("QFeatures")
-        library(QFeatures)
-        
-        if (!require("purrr")) install.packages("purrr")
-        library(purrr)
-        
-        if (!require("dplyr")) install.packages("dplyr")
-        library(dplyr)
-        
-        if (!require("ggplot2")) install.packages("ggplot2")
-        library(ggplot2)
-        
-        if (!require("M3C")) BiocManager::install("M3C")
-        library(M3C)
-        
-        if (!require("yaml")) install.packages("yaml")
-        library(yaml)
-    }
-    
-    # Load all helperfunctions
-    source("R/helperfunctions.R")
-    source("R/librarysearch_se.R")
-    source("R/slaw2summarizedExperiment.R")
-    
-    # Read in settings of yaml file
-    settings <- read_yaml(settings_yaml)
-    
-    # Generate output directory in project folder
-    if(!dir.exists(settings$output_dir)) dir.create(settings$output_dir)
-    
-    # Store yaml file in output directory
-    write_yaml(settings, paste0(settings$output_dir, "/settings_MetaboAnnotation.yaml"))
-        
-    return(settings)
+# Load all required packages
+{
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  
+  if (!require("MetaboAnnotation")) devtools::install_github("rformassspectrometry/MetaboAnnotation")
+  library(MetaboAnnotation)
+  
+  if (!require("MsBackendMassbank")) BiocManager::install("MsBackendMassbank")
+  library(MsBackendMassbank)
+  
+  if (!require("MsBackendMgf")) BiocManager::install("MsBackendMgf")
+  library(MsBackendMgf)
+  
+  if (!require("MsBackendMsp")) BiocManager::install("MsBackendMsp")
+  library(MsBackendMsp)
+  
+  if (!require("Spectra")) BiocManager::install("Spectra")
+  library(Spectra)
+  
+  if (!require("QFeatures")) BiocManager::install("QFeatures")
+  library(QFeatures)
+  
+  if (!require("purrr")) install.packages("purrr")
+  library(purrr)
+  
+  if (!require("dplyr")) install.packages("dplyr")
+  library(dplyr)
+  
+  if (!require("ggplot2")) install.packages("ggplot2")
+  library(ggplot2)
+  
+  if (!require("M3C")) BiocManager::install("M3C")
+  library(M3C)
+  
+  if (!require("yaml")) install.packages("yaml")
+  library(yaml)
+  
+  if(!require("stringr")) install.packages("stringr")
+  library(stringr)
 }
+
+# Load all helperfunctions
+source("R/helperfunctions.R")
+source("R/librarysearch_se.R")
+source("R/slaw2summarizedExperiment.R")
--- R/03_MS1Annotation.R
+++ R/03_MS1Annotation.R
@@ -4,7 +4,101 @@
 #' @param output Where to store annotated Output
 #' @param settings File containg settings
 #' 
-MS1_annotation <- function(se, output, settings){
+perform_ms1_annotation <- function(se,
+                                   libpath,
+                                   adducts,
+                                   tolerance = 0.000,
+                                   ppm = 5,
+                                   toleranceRt = NA,
+                                   outputdir = NA,
+                                   ionmode = "",
+                                   saveRds = TRUE,
+                                   saveTsv = FALSE) {
+  
+  # build param object based on RT selectoin
+  if(is.na(toleranceRt)) {
     
+    param <- Mass2MzParam(adducts = adducts,
+                          tolerance = tolerance,
+                          ppm = ppm)
     
+  } else {
+    
+    param <- Mass2MzRtParam(adducts = adducts,
+                            tolerance = tolerance,
+                            ppm = ppm,
+                            toleranceRt = toleranceRt)
+    
+  }
+  
+  # perform matching for each compound library in libpath
+  ms1_libraries <- list.files(libpath, full.names = TRUE)
+  
+  for(ms1_library in ms1_libraries) {
+    
+    # read library data and perform some sanity checks
+    ms1_lib_data <- read.delim(ms1_libraries)
+    
+    # check if all required columns are in place
+    if(class(param) == "Mass2MzParam" && !all(c("id", "name", "formula", "exactmass") %in% colnames(ms1_lib_data))) {
+      
+      message(paste0("Missing one or all required columns id, name, formula, exactmass in library ", basename(ms1_library)))
+      next
+      
+    } else if(class(param) == "Mass2MzRtParam" && !all(c("id", "name", "formula", "exactmass", "rt") %in% colnames(ms1_lib_data))) {
+      
+      message(paste0("Missing one or all required columns id, name, formula, exact_mass, rt in library ", basename(ms1_library)))
+      next
+      
+    }
+    
+    # perform annotation
+    se_match <- matchMz(rowData(se)[[1]],
+                        ms1_lib_data,
+                        param = param)
+    
+    # save results in a rds file
+    if(saveRds && class(param) == "Mass2MzParam") {
+      
+      saveRDS(se_match,
+              paste0(outputdir,
+                     "/Annotation_MS1_external/",
+                     ionmode,
+                     "_",
+                     str_replace(basename(ms1_library), ".tsv$", ""),
+                     "_ms1annotation.rds"))
+      
+    } else if(saveRds && class(param) == "Mass2MzRtParam") {
+      
+      saveRDS(se_match,
+              paste0(outputdir,
+                     "/Annotation_MS1_inhouse/",
+                     ionmode,
+                     "_",
+                     str_replace(basename(ms1_library), ".tsv$", ""),
+                     "_ms1annotation.rds"))
+      
+    }
+    
+    # save results in a rds file
+    if(saveTsv && class(param) == "Mass2MzParam") {
+      
+      write.table(matchedData(se_match),
+                  paste0(outputdir,
+                         "/Annotation_MS1_external/",
+                         ionmode,
+                         "_",
+                         basename(ms1_library)))
+      
+    } else if(saveTsv && class(param) == "Mass2MzRtParam") {
+      
+      write.table(matchedData(se_match),
+                  paste0(outputdir,
+                         "/Annotation_MS1_inhouse/",
+                         ionmode,
+                         "_",
+                         basename(ms1_library)))
+      
+    }
+  }
 }
